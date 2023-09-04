ea6a05cc0f9e1bded53ff05632a9ad2b636d9bb7
manuel-alvarez-chaves <manuel.alvarez-chaves@simtech.uni-stuttgart.de>
2023-09-04 15:10:22 +0200

    Small fix from previous commit.

--- unite_toolbox/utils.py
+++ unite_toolbox/utils.py
@@ -1,29 +1,28 @@
 import numpy as np
 
-def get_samples(func, limits, n_samples, **kwargs):
-    """
-    """
+def get_samples(func, limits, n_samples, seed=None, **kwargs):
     
+    rng = np.random.default_rng(seed)
+
     ids = []
     acc_rate = 1.0
     while len(ids) < n_samples:
         d = len(limits)
         f = np.array(limits)[:, 0] # floor
         s = np.array(limits)[:, 1] - f # scale
-        r = np.random.uniform(size=(int(n_samples / acc_rate), d))
+        r = rng.uniform(size=(int(n_samples / acc_rate), d))
         r = f + s * r
             
         F = func(*(np.hsplit(r, d)), **kwargs).flatten()
         G = 1 / np.prod(s)
         M = F.max() / G
 
-        U = np.random.uniform(0.0, 1.0, size=F.shape)
+        U = rng.uniform(0.0, 1.0, size=F.shape)
         ids = np.argwhere(U < F/(M * G)).flatten()
         acc_rate = acc_rate * (len(ids) / n_samples)
 
     samples = r[ids][:n_samples, :]
     return samples
-
 
 def power_set(items):
     """Creates a list of lists which are the power set of the input
