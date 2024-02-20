491530de8ad0c093b07aa5d02cb088f68af31024
manuel-alvarez-chaves <manuel.alvarez-chaves@simtech.uni-stuttgart.de>
2024-02-20 17:09:31 +0100

    Increased version to solve installation problems

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="unite_toolbox",
-    version="0.1",
+    version="0.1.3",
     contact_email="malvarez062@gmail.com",
     packages=find_packages(),
     install_requires=["numpy >= 1.25.0", "scipy >= 1.10.1"],
