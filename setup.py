45b0008db19b6af59902c8e6df4d08d89ba269e8
manuel-alvarez-chaves <manuel.alvarez-chaves@simtech.uni-stuttgart.de>
2024-02-26 11:42:08 +0100

    Ignored .bak files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="unite_toolbox",
-    version="0.1.3",
-    contact_email="malvarez062@gmail.com",
-    packages=find_packages(),
-    install_requires=["numpy >= 1.25.0", "scipy >= 1.10.1"],
-)
