06da0d369d8f0d4a1fed465ed66f21757bdb415e
manuel-alvarez-chaves <manuel.alvarez-chaves@simtech.uni-stuttgart.de>
2024-03-04 18:49:27 +0100

    Bin density available for multi-dimensional data

--- unite_toolbox/bin_estimators.py
+++ unite_toolbox/bin_estimators.py
@@ -1,16 +1,5 @@
 import numpy as np
 
-def calc_bin_density(x, data):
-    """Currently only for 1D"""
-    
-    fi, edges = np.histogram(data, "scott", density=True)
-
-    fx = np.zeros(shape=x.shape)
-    x_bins = np.digitize(x, edges)
-    idx = np.bitwise_and(x_bins != 0, x_bins != len(edges))
-    fx[idx] = fi[x_bins[idx] - 1]
-    
-    return fx
 
 def estimate_ideal_bins(data, counts=True):
     """
@@ -27,35 +16,83 @@ def estimate_ideal_bins(data, counts=True):
     Returns
     -------
     dict
-        A dictionary with a key for each method, and the values are lists of 
-        number of bins or bin edges for each feature of the data.
+        A dictionary with a key for each method, and the values are lists of
+        number of bins or bin edges for each feature of the data (if counts=False).
     """
-    
+
     _, n_features = data.shape
-    
+
     methods = ["scott", "fd", "sturges"]
     ideal_bins = []
-    
+
     for m in methods:
         d_bins = []
         for d in range(n_features):
             num_bins = np.histogram_bin_edges(data[:, d], bins=m)
-            num_bins = len(num_bins) if counts == True else num_bins              
+            num_bins = len(num_bins) if counts is True else num_bins
             d_bins.append(num_bins)
         ideal_bins.append(d_bins)
-            
+
     return dict(zip(methods, ideal_bins))
+
+
+def calc_bin_density(x, data, edges):
+    """
+    Calculates the density of every point of the 2D array x within the d-dimensional
+    histogram created from data and edges.
     
+    Similar to a lookup operation where the
+    entries in x are replaced by the bin indices in which they would fall given the
+    binning scheme defined in edges. Then the indices are used to "look up" each value
+    of x in the d-dimensional histogram created from data and edges.
+
+    arameters
+    ----------
+    x : numpy.ndarray
+        Array of shape (n_samples, d_features)
+    data : numpy.ndarray
+        Array of shape (n_samples, d_features)
+    edges : list or int
+        A list of length d_features which contains arrays describing the bin edges
+        along each dimension or a list of ints describing the number of bins to use
+        in each dimension.
+
+    Returns
+    -------
+    fx : numpy.ndarray
+        Array of shape (n_samples, 1)
+    """
+    fi, edges = np.histogramdd(data, edges, density=True)
+    res = []
+    # Loop over dimensions
+    for i in range(len(edges)):
+        dimbins = np.digitize(x[:, i], edges[i])
+        dimbins = (
+            np.where((dimbins > 0) & (dimbins < len(edges[i])), dimbins, -9998) - 1
+        )
+        res.append(dimbins)
+
+    # Loop over elements
+    indexes = np.vstack(res).T
+    fx = np.zeros(shape=(x.shape[0], 1))
+    for i, idx in enumerate(indexes):
+        if -9999 in idx:
+            pass
+        else:
+            fx[i, 0] += fi[tuple(idx)]
+    return fx
+
+
 def calc_vol_array(edges):
-    """
-    """
-    
+    """ """
+
     res = edges[0]
     for e in edges[1:]:
-        res = np.stack([res]*len(e), axis=-1)
+        res = np.stack([res] * len(e), axis=-1)
         for id, val in enumerate(e):
             res[..., id] = res[..., id] * val
     return res
+
 
 def calc_bin_entropy(data, edges):
     """
@@ -65,7 +102,7 @@ def calc_bin_entropy(data, edges):
     Parameters
     ----------
     data : numpy.ndarray
-        Array of shape (n_samples, n_features)
+        Array of shape (n_samples, d_features)
     edges : list or int
         A list of length n_features which contains arrays describing the bin edges
         along each dimension or a list of ints describing the number of bins to use
@@ -87,7 +124,7 @@ def calc_bin_entropy(data, edges):
     # volume
     edges = [np.diff(e) for e in edges]
     volume = calc_vol_array(edges)
-    
+
     # entropy
     ids = fi.nonzero()
     delta = volume[ids]
@@ -97,10 +134,10 @@ def calc_bin_entropy(data, edges):
 
     return h, cf
 
+
 def calc_bin_kld(data_f, data_g, edges):
-    """
-    """
-    
+    """ """
+
     fi, _ = np.histogramdd(data_f, bins=edges, density=True)
     gi, edges = np.histogramdd(data_g, bins=edges, density=True)
 
@@ -113,15 +150,16 @@ def calc_bin_kld(data_f, data_g, edges):
     fi, gi, delta = fi[ids], gi[ids], delta[ids]
 
     kld = np.sum(fi * delta * np.log(fi / gi))
-    
+
     return kld
-    
+
+
 def calc_bin_mutual_information(data, edges):
     _, d = data.shape
 
-    data_x = data[:, :d-1].reshape(-1, d-1)
-    edges_x = edges[:d-1]
-    data_y = data[:, d-1].reshape(-1, 1)
+    data_x = data[:, : d - 1].reshape(-1, d - 1)
+    edges_x = edges[: d - 1]
+    data_y = data[:, d - 1].reshape(-1, 1)
     edges_y = edges[-1]
 
     fxy, bin_edges = np.histogramdd(data, bins=edges, density=True)
@@ -134,17 +172,22 @@ def calc_bin_mutual_information(data, edges):
     mi = 0.0
     for idxy in np.ndindex(fxy.shape):
         if fxy[idxy] != 0.0:
-            mi += fxy[idxy] * volume[idxy] * np.log(fxy[idxy] / (fx[idxy[:d-1]] * fy[idxy[-1]]))
-            
+            mi += (
+                fxy[idxy]
+                * volume[idxy]
+                * np.log(fxy[idxy] / (fx[idxy[: d - 1]] * fy[idxy[-1]]))
+            )
+
     return max(0, mi)
-    
+
+
 def calc_qs_entropy(sample, alpha=0.25, N_k=500):
     """Calculates the 1-D entropy of the input data.
 
     Calculates the 1-D entropy of the input data [in nats] using
     the  quantile spacing (QS) estimator proposed by: Gupta et al.
     (2021) https://doi.org/10.3390/e23060740
-    
+
     Adapted from: https://github.com/rehsani/Entropy
 
 
@@ -153,12 +196,12 @@ def calc_qs_entropy(sample, alpha=0.25, N_k=500):
     data : numpy.ndarray
         Flat array
     alpha : float, optional
-        percent of the instances from the sample used for estimation of 
+        percent of the instances from the sample used for estimation of
         entropy (i.e., number of quantile-spacings).
     N_k : int, optional
-        number of sample subsets, used to estimate the sample distribution 
+        number of sample subsets, used to estimate the sample distribution
         for each quantile empirically
-    
+
     Returns
     -------
     h : float
@@ -171,7 +214,9 @@ def calc_qs_entropy(sample, alpha=0.25, N_k=500):
     sample.sort()
 
     sample_b = np.random.choice(sample, sample.size, replace=True)
-    X_alpha = [np.random.choice(sample_b[1:-1], n-1, replace=False) for _ in range(N_k)]
+    X_alpha = [
+        np.random.choice(sample_b[1:-1], n - 1, replace=False) for _ in range(N_k)
+    ]
     X_alpha = np.vstack(X_alpha)
     X_alpha.sort(axis=1)
     Z = np.hstack([x_min, X_alpha.mean(axis=0), x_max])
