1f175465d91f9fe3617155cab72a75fa6475bdb9
manuel-alvarez-chaves <manuel.alvarez-chaves@simtech.uni-stuttgart.de>
2024-04-11 14:24:46 +0200

    Moved find_repeats and add_noise to data_validation module

--- unite_toolbox/utils/data_validation.py
+++ unite_toolbox/utils/data_validation.py
@@ -1,5 +1,61 @@
 import numpy as np
-from unite_toolbox.utils.bootstrapping import find_repeats
+from scipy import stats
+
+
+def find_repeats(data):
+    """Returns a boolean mask for repeat rows in data
+    where True is a repeated row.
+
+    Parameters
+    ----------
+    data : array_like
+        2D array like of shape (n_samples, d_features)
+
+    Returns
+    -------
+    mask : numpy.ndarray
+        Boolean array of shape (n_samples,)"""
+
+    data = np.asarray(data, dtype=np.float64)
+    _, inv, counts = np.unique(data, return_inverse=True, return_counts=True, axis=0)
+    mask = np.where(counts[inv] > 1, True, False)
+    return mask
+
+
+def add_noise_to_data(data):
+    """Adds noise to repeated rows in data.
+
+    Adds Gaussian noise to only the repeated rows in a 2D array.
+    The noise added is one order of magnitude below the order of magnitude
+    of the std. dev. of each specific column in the data. This was empirically
+    determined to be adequate for distance based measures.
+
+    Parameters
+    ----------
+    data : array_like
+        2D array like of shape (n_samples, d_features)
+
+    Returns
+    -------
+    noisy_data : numpy.ndarray
+        2D array of shape (n_samples, d_features)"""
+
+    data = np.asarray(data, dtype=np.float64)
+    _, d = data.shape
+
+    # Determine the scale of the noise
+    data_std = np.std(data, axis=0).reshape(1, -1) / 10
+    noise_scale = 10 ** (np.floor(np.log10(np.abs(data_std))))
+
+    # Generate only the required noise for the data
+    mask = find_repeats(data)
+    noise = stats.multivariate_normal.rvs(cov=np.diag(noise_scale.flatten()), size=mask.sum(), random_state=42).reshape(-1, d)
+
+    # Add noise to specific rows
+    noisy_data = data.copy()
+    noisy_data[mask] += noise
+
+    return noisy_data
 
 
 def valida_data_kld(a, b, verbose=False):
