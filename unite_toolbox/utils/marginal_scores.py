f120134d7add220c42ffa6bb0c9b74e7aea74fd1
manuel-alvarez-chaves <malvarez062@gmail.com>
2024-05-06 18:31:39 +0200

    Documentation and small fixes

--- unite_toolbox/utils/marginal_scores.py
+++ unite_toolbox/utils/marginal_scores.py
@@ -1,34 +1,58 @@
-import numpy as np
+from collections.abc import Iterable
 from itertools import chain, combinations
+from typing import Any
+
+import numpy as np
 
 
-def power_set(items: list) -> list[tuple]:
+def power_set(items: Iterable) -> list[tuple]:
     """Create power set.
 
-    Creates a list of tuples which are the power set of the input items.
+    Creates a list of tuples which are the power set (all possible
+    combinations) of the input items.
     Based on the itertools recipe:
     https://docs.python.org/3/library/itertools.html#itertools-recipes
 
     Parameters
     ----------
-    items : list
-        List of items
+    items : Iterable
+        Iterable object containing individual labels
 
     Returns
     -------
     power_set : list[tuples]
         List of tuples containing each element of the power set
+
     """
+    s = list(items)
     power_set = chain.from_iterable(
-        combinations(items, r) for r in range(len(items) + 1)
+        combinations(s, r) for r in range(len(s) + 1)
     )
     return list(power_set)
 
 
-def calc_marginal_scores(scores: dict) -> dict:
+def calc_marginal_scores(scores: dict[tuple, float]) -> dict[Any, float]:
     """Calculate marginal scores due to adding more features.
 
-    (blah)
+    Calculates the marginal score of adding a specific feature to a callable
+    that generates a score. That callable can be a function (e.g. mutual
+    information) or a model. The result is a marginal gain (or loss) of
+    adding an individual feature as input. The combinations of features are
+    the tuples and each tuple has a specific score associated. The result is
+    a dict of individual features as keys and their marginal score as values.
+
+    Parameters
+    ----------
+    scores : dict
+        Dict. of tuples of feature combinations and individual scores
+        i.e. {(): 0.0, ("a"): 0.3, ("b"): 0.5, ("a", "b"): 0.8}
+
+    Returns
+    -------
+    marginal_scores : dict
+        Dict. of individual labels and their marginal scores
+        i.e. {"a": 0.15, "b": 0.25}
+
     """
     # Preparation data
     combs = list(scores.keys())
